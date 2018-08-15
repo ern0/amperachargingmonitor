c1e4fecc4f8fe8a87220a5192fa74bbdd1b5a140
ern0 <ern0@linkbroker.hu>
2018-08-15 07:41:06 +0200

    started analyzer

--- findgreendot.py
+++ findgreendot.py
@@ -21,13 +21,8 @@ class FindGreenDot:
 		self.checkFileExistence(fnam)
 		
 		self.frameCount = self.countNumberOfFrames()
-		
-		while True:
-			
-			self.extractFrame(40)
-			self.procImage()
-			break;
-
+		self.countStrokes()
+	
 
 	def fatal(self,msg):
 		sys.stderr.write("ERROR: " + str(msg) + "\n")
@@ -93,19 +88,63 @@ class FindGreenDot:
 		
 		image = Image.open(self.mkTmpImgPath())
 		pixels = image.load()
-		
 		(width,height) = image.size
-		
-		for x in range(0,width):
-			for y in range(0,height):
-				
-				(r,g,b) = pixels[x,y]
 
-				if g < 50: continue
-				if g < 50: continue
-				
-				print(x,y,g)	
+		deltaRMax = 0
+		deltaBMax = 0
 		
+		for y in range(0,height):
+			for x in range(0,width):				
+
+				(r,g,b) = pixels[x,y]
+				deltaR = g - r
+				deltaB = g - b
+				
+				if deltaR > deltaRMax: deltaRMax = deltaR			
+				if deltaB > deltaBMax: deltaBMax = deltaB
+						
+		found = 0
+		
+		for y in range(0,height):
+			for x in range(0,width):				
+
+				(r,g,b) = pixels[x,y]
+				deltaR = g - r
+				deltaB = g - b
+				
+				if deltaR > deltaRMax / 2: found += 1
+				if deltaB > deltaBMax / 2: found += 1
+		
+		return found
+
+
+	def countStrokes(self):
+		
+		strokeType = -1		 	
+		strokeCount = [0,0]
+		strokeLength = [0,0]
+				
+		for i in range(0,self.frameCount):
+			
+			print("Frame",i,"/",self.frameCount)
+			
+			self.extractFrame(i)
+			found = self.procImage()
+			if found: value = 1
+			else: value = 0
+			
+			if strokeType != value:
+				strokeType = value				
+				strokeCount[strokeType] += 1
+			
+			strokeLength[strokeType] += 1
+				
+		for i in range(0,1):
+			if strokeCount[i] == 0: continue
+			strokeLength[i] = strokeLength[i] / strokeCount[i]
+			
+		print(strokeCount,strokeLength)
+
 
 if __name__ == '__main__':
 
--- s
+++ s
@@ -1,3 +1,5 @@
 #!/bin/bash
+clear
 
-./findgreendot.py sample/dark-light.avi tmp
+#./findgreendot.py sample/dark-light.avi tmp
+./findgreendot.py sample/dark-blink.avi tmp
