eca1415a9314f479574649693d69501d5634c236
ern0 <ern0@linkbroker.hu>
2018-08-31 17:05:27 +0200

    refactor: split to methods

--- findgreendot.py
+++ findgreendot.py
@@ -96,62 +96,95 @@ class FindGreenDot:
 		).read()
 
 
-	def procImage(self,fnam):
+	def procImage(self,fnam,way = "hard"):
 
 		source = Image.open(self.mkTmpImgPath(fnam))
-		(width,height) = source.size
-		pixels = source.load()
+		(self.width,self.height) = source.size
+		self.pixels = source.load()
 
 		target = Image.new(
 			"RGB",
-			(width * 2 + 1, height),
+			(self.width * 2 + 1, self.height),
 			"black"
 		)
-		result = target.load()
+		self.result = target.load()
 
 		# copy source and draw separator
 
 		if self.debugMode:
-			for y in range(0,height):
-				result[width,y] = (0,255,0)
-				for x in range(0,width):
-					result[x + width + 1, y] = pixels[x,y]
+			for y in range(0,self.height):
+				self.result[self.width,y] = (0,255,0)
+				for x in range(0,self.width):
+					self.result[x + self.width + 1, y] = self.pixels[x,y]
 
-		# copy and analyze 3x3 region of the image
+		# perform the job
 
-		CHANGE_SMALL = 0
-		CHANGE_LIGHTER = 1
-		CHANGE_DARKER = 2
-		CHANGE_FILL = 3
-		FILL_OFF = -1
+		found = -1
+		if way == "hard": found = self.procImageTheHardWay()
+		else: found = self.procImageTheEasyWay()
+
+		# save image for debugging purposes
+
+		if self.debugMode:
+			target.save("/tmp/image.png","PNG")
+
+		return found
+
+
+	def procImageTheHardWay(self):
+
+		self.hardSetEnums()
+		self.hardFindEdges()
+		self.hardFillGaps()
+		self.hardCollectSpots()
+		self.hardCalcBoundingRects()
+		self.hardFindMatchingSpots()
+		self.hardDrawBoundingBoxes()
+
+		return self.found
+
+
+	def hardSetEnums(self):
+
+		self.CHANGE_SMALL = 0
+		self.CHANGE_LIGHTER = 1
+		self.CHANGE_DARKER = 2
+		self.CHANGE_FILL = 3
+		self.FILL_OFF = -1
+
+
+	def hardFindEdges(self):
+
+		# create placeholder 3x3 matrix
 
 		mx = [
 			[ (0,0,0), (0,0,0), (0,0,0) ],
 			[ (0,0,0), (0,0,0), (0,0,0) ],
 			[ (0,0,0), (0,0,0), (0,0,0) ]
 		]
-		for y in range(1,height - 1):
-			for x in range(1,width - 1):
 
-				# copy 3x3 region
+		# foreach image-pixel
 
-				mx[0][0] = pixels[x - 1, y - 1]
-				mx[0][1] = pixels[x    , y - 1]
-				mx[0][2] = pixels[x + 1, y - 1]
+		for y in range(1,self.height - 1):
+			for x in range(1,self.width - 1):
 
-				mx[1][0] = pixels[x - 1, y]
-				mx[1][1] = pixels[x    , y]
-				mx[1][2] = pixels[x + 1, y]
+				# copy 3x3 region (known issue: not used now)
 
-				mx[2][0] = pixels[x - 1, y + 1]
-				mx[2][1] = pixels[x    , y + 1]
-				mx[2][2] = pixels[x + 1, y + 1]
+				mx[0][0] = self.pixels[x - 1, y - 1]
+				mx[0][1] = self.pixels[x    , y - 1]
+				mx[0][2] = self.pixels[x + 1, y - 1]
+
+				mx[1][0] = self.pixels[x - 1, y]
+				mx[1][1] = self.pixels[x    , y]
+				mx[1][2] = self.pixels[x + 1, y]
+
+				mx[2][0] = self.pixels[x - 1, y + 1]
+				mx[2][1] = self.pixels[x    , y + 1]
+				mx[2][2] = self.pixels[x + 1, y + 1]
 
 				# calc sobel-ish difference
 
-				diff = (
-					mx[1][2][1] - mx[1][0][1]
-				)
+				diff = mx[1][2][1] - mx[1][0][1]
 
 				# filter for only green
 
@@ -163,46 +196,53 @@ class FindGreenDot:
 				if diff > 0:
 
 					if abs(diff) > self.SOBEL_DIFF_LIGHTER:
-						result[x,y] = (1,255,CHANGE_LIGHTER)
+						self.result[x,y] = (1,255,self.CHANGE_LIGHTER)
 					else:
-						result[x,y] = (0,0,CHANGE_SMALL)
+						self.result[x,y] = (0,0,self.CHANGE_SMALL)
 
 				else:
 
 					if abs(diff) > self.SOBEL_DIFF_DARKER:
-						result[x,y] = (255,1,CHANGE_DARKER)
+						self.result[x,y] = (255,1,self.CHANGE_DARKER)
 					else:
-						result[x,y] = (0,0,CHANGE_SMALL)
+						self.result[x,y] = (0,0,self.CHANGE_SMALL)
 
-		# fill gaps
 
-		for y in range(1,height - 1):
-			fill = FILL_OFF
-			for x in range(1,width - 1):
-				pix = result[x,y]
+	def hardFillGaps(self):
 
-				if fill == FILL_OFF and pix[2] == CHANGE_LIGHTER:
+		for y in range(1,self.height - 1):
+			fill = self.FILL_OFF
+			for x in range(1,self.width - 1):
+				pix = self.result[x,y]
+
+				if fill == self.FILL_OFF and pix[2] == self.CHANGE_LIGHTER:
 					fill = x + 1
 
-				if fill != FILL_OFF and pix[2] == CHANGE_DARKER:
+				if fill != self.FILL_OFF and pix[2] == self.CHANGE_DARKER:
 					if x - fill < 15:
-						for gap in range(fill,x): result[gap,y] = (255,255,CHANGE_FILL)
-					fill = FILL_OFF
+						for gap in range(fill,x): self.result[gap,y] = (255,255,self.CHANGE_FILL)
+					fill = self.FILL_OFF
 
-		# collect spots
 
-		xyOffsets = [  # neighbour offset x,y pairs for later use
+	def hardCollectSpots(self):
+
+		# neighbour offset x,y pairs for later use
+
+		xyOffsets = [
 			(-1,-2), ( 0,-2), (+1,-2),
 			( 0,-1), ( 0,-1), (+1,-1),
 			(-1,0)
 		]
 
-		spotMap = {}  # spotMap[ tupleof(x,y) ] = spot ID
-		nextSpotId = 1  # initial spot ID
+		# spotMap[ tupleof(x,y) ] = spot ID
+		self.spotMap = {}
 
-		for y in range(2,height - 2):
-			for x in range(2,width - 2):
-				if result[x,y][2] == CHANGE_SMALL: continue
+		# initial spot ID
+		nextSpotId = 1
+
+		for y in range(2,self.height - 2):
+			for x in range(2,self.width - 2):
+				if self.result[x,y][2] == self.CHANGE_SMALL: continue
 
 				# add current diff-pixel to an existing spot, or create a new one
 
@@ -213,14 +253,14 @@ class FindGreenDot:
 				for xyOffset in xyOffsets:
 					xy = ( x + xyOffset[0], y + xyOffset[1] )
 
-					if xy not in spotMap: continue
-					spotId = spotMap[xy]
+					if xy not in self.spotMap: continue
+					spotId = self.spotMap[xy]
 					neighbourSpots[spotId] = None
 
 				# if there is no neighbour, register new spot
 
 				if len(neighbourSpots) == 0:
-					spotMap[x,y] = nextSpotId
+					self.spotMap[x,y] = nextSpotId
 					nextSpotId += 1
 
 				# else make union for all spots touched as neighbour
@@ -231,53 +271,56 @@ class FindGreenDot:
 
 					# change all neighbours' ID in spotMap to union ID
 					for obsoleteSpotId in neighbourSpots:
-						for xy in spotMap:
-							if spotMap[xy] != obsoleteSpotId: continue
-							spotMap[xy] = unionId
+						for xy in self.spotMap:
+							if self.spotMap[xy] != obsoleteSpotId: continue
+							self.spotMap[xy] = unionId
 
 					# set current diff-pixel ID to union ID
-					spotMap[x,y] = unionId
+					self.spotMap[x,y] = unionId
 
-		# get bounding rectangles of collected spots
 
-		spotTops = {}
-		spotBottoms = {}
-		spotLefts = {}
-		spotRights = {}
-		spotPixels = {}
-		for spotCoords in spotMap:
-			spotId = spotMap[spotCoords]
+	def hardCalcBoundingRects(self):
 
-			if spotId not in spotPixels:
-				spotTops[spotId] = spotCoords[1]
-				spotBottoms[spotId] = spotCoords[1]
-				spotLefts[spotId] = spotCoords[0]
-				spotRights[spotId] = spotCoords[0]
-				spotPixels[spotId] = 0
+		self.spotTops = {}
+		self.spotBottoms = {}
+		self.spotLefts = {}
+		self.spotRights = {}
+		self.spotPixels = {}
+		for spotCoords in self.spotMap:
+			spotId = self.spotMap[spotCoords]
 
-			if spotCoords[1] < spotTops[spotId]: spotTops[spotId] = spotCoords[1]
-			if spotCoords[1] > spotBottoms[spotId]: spotBottoms[spotId] = spotCoords[1]
-			if spotCoords[0] < spotLefts[spotId]: spotLefts[spotId] = spotCoords[0]
-			if spotCoords[0] > spotRights[spotId]: spotRights[spotId] = spotCoords[0]
-			spotPixels[spotId] += 1
+			if spotId not in self.spotPixels:
+				self.spotTops[spotId] = spotCoords[1]
+				self.spotBottoms[spotId] = spotCoords[1]
+				self.spotLefts[spotId] = spotCoords[0]
+				self.spotRights[spotId] = spotCoords[0]
+				self.spotPixels[spotId] = 0
 
-		# find spots which matches our criterias
+			if spotCoords[1] < self.spotTops[spotId]: self.spotTops[spotId] = spotCoords[1]
+			if spotCoords[1] > self.spotBottoms[spotId]: self.spotBottoms[spotId] = spotCoords[1]
+			if spotCoords[0] < self.spotLefts[spotId]: self.spotLefts[spotId] = spotCoords[0]
+			if spotCoords[0] > self.spotRights[spotId]: self.spotRights[spotId] = spotCoords[0]
+			self.spotPixels[spotId] += 1
 
-		found = 0
-		for spotId in spotPixels:
+
+	def hardFindMatchingSpots(self):
+
+		self.found = 0
+		self.match = {}
+		for spotId in self.spotPixels:
 
 			# calculate bounding rectangle width and height
 			# we used horizontal sobel, so width is corrected
 
-			w = spotRights[spotId] - spotLefts[spotId] + 1
-			h = spotBottoms[spotId] - spotTops[spotId] + 1
+			w = self.spotRights[spotId] - self.spotLefts[spotId] + 1
+			h = self.spotBottoms[spotId] - self.spotTops[spotId] + 1
 			correctedW = w - 1
 
-			match = True
+			self.match[spotId] = True
 
 			# pre-calculate some values for debugging
 
-			fillRatio = spotPixels[spotId] / (w * h)
+			fillRatio = self.spotPixels[spotId] / (w * h)
 			if correctedW == 0 or h == 0:
 				ratio = None
 				printableRatio = "n.a."
@@ -289,15 +332,15 @@ class FindGreenDot:
 
 			if self.debugMode: print(
 				"TL:",
-				str(spotLefts[spotId]) + ";" +
-				str(spotTops[spotId]),
+				str(self.spotLefts[spotId]) + ";" +
+				str(self.spotTops[spotId]),
 				"dim:",
 				str(w) + "x" + str(h),
 				"(" +	str(correctedW) + "x" + str(h) + ")",
 				"ratio:",
 				printableRatio,
 				"filled:",
-				str(spotPixels[spotId]) + "/" + str(w * h),
+				str(self.spotPixels[spotId]) + "/" + str(w * h),
 				str(round(fillRatio * 10000) / 100) + "%",
 				end = " -"
 			)
@@ -306,63 +349,60 @@ class FindGreenDot:
 
 			if correctedW < self.MIN_SIZE_PX or h < self.MIN_SIZE_PX:
 				if self.debugMode: print(" small_size",end="")
-				match = False
+				self.match[spotId] = False
 
 			# drop ones with weak fill ratio, e.g. diagonal lines
 			# (using uncorrected width for fill ratio)
 
 			if fillRatio < self.MIN_FILL_RATIO:
 				if self.debugMode: print(" fill_ratio",end="")
-				match = False
+				self.match[spotId] = False
 
 			# drop ones with only few pixels filled
 
-			if spotPixels[spotId] < self.MIN_FILL_PIX:
+			if self.spotPixels[spotId] < self.MIN_FILL_PIX:
 				if self.debugMode: print(" fill_number",end="")
-				match = False
+				self.match[spotId] = False
 
 			# drop non-square-ish shapes
 
 			if ratio is not None:
 				if ratio < self.MIN_SQUARE_RATIO:
 					if self.debugMode: print(" not_square",end="")
-					match = False
+					self.match[spotId] = False
 
 			# checkpoint, report if no matching failed
 
-			if match:
-				found += 1
+			if self.match[spotId]:
+				self.found += 1
 				if self.debugMode: print(" PASSED.")
 			else:
 				if self.debugMode: print()
 
-			# draw bounding boxes
 
-			if self.debugMode:
-				for y in range(spotTops[spotId] - 1,spotBottoms[spotId] + 2):
-					for x in range(spotLefts[spotId] - 1,spotRights[spotId] + 2):
-						if result[x,y][2] != CHANGE_SMALL: continue
+	def hardDrawBoundingBoxes(self):
+
+			if not self.debugMode: return
+
+			for spotId in self.spotPixels:
+
+				for y in range(self.spotTops[spotId] - 1,self.spotBottoms[spotId] + 2):
+					for x in range(self.spotLefts[spotId] - 1,self.spotRights[spotId] + 2):
+						if self.result[x,y][2] != self.CHANGE_SMALL: continue
 
 						extra = False
-						if y < spotTops[spotId]: extra = True
-						if y > spotBottoms[spotId]: extra = True
-						if x < spotLefts[spotId]: extra = True
-						if x > spotRights[spotId]: extra = True
+						if y < self.spotTops[spotId]: extra = True
+						if y > self.spotBottoms[spotId]: extra = True
+						if x < self.spotLefts[spotId]: extra = True
+						if x > self.spotRights[spotId]: extra = True
 
-						if match:
-							if extra: result[x,y] = (192,192,255)
-							else: result[x,y] = (0,0,255)
+						if self.match[spotId]:
+							if extra: self.result[x,y] = (192,192,255)
+							else: self.result[x,y] = (0,0,255)
 
 						else:
-							if extra: result[x,y] = (127,127,127)
-							else: result[x,y] = (63,63,63)
-
-		# save image for debugging purposes
-
-		if self.debugMode:
-			target.save("/tmp/image.png","PNG")
-
-		return found
+							if extra: self.result[x,y] = (127,127,127)
+							else: self.result[x,y] = (63,63,63)
 
 
 	def procFrames(self,fnam,specifiedFrame = None):
