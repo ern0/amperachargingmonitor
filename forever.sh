3ee75653640f2db30862ff734ca8cbb5cf305749
ern0 <ern0@linkbroker.hu>
2019-10-29 19:31:32 +0100

    added tmp delete

--- forever.sh
+++ forever.sh
@@ -6,7 +6,7 @@ dir=/mnt/ram
 while true
 do
   mkdir -p $dir/result
-	rm -rf $dir/tmp-*
+  rm -rf $dir/tmp-*
   t=$dir/tmp-`date -Iseconds | cut -d+ -f1 `.txt
   f=$dir/result/`date -Iseconds | cut -d+ -f1 `.txt
   ./runme.sh > $t
